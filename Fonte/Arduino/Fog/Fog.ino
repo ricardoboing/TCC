7929e3c3f612c1f222d45736cfafd367a83a4b3c
Ricardo Boing <ricardoboing.ufsc@gmail.com>
2019-05-07 17:00:19 -0300

    Remocao de codigo de testes (copiado de outro autor);

--- Fonte/Arduino/Fog/Fog.ino
+++ Fonte/Arduino/Fog/Fog.ino
@@ -1,33 +1,30 @@
-#include <SPI.h>
-#include "nRF24L01.h"
-#include "RF24.h"
+#include "Antena.hpp"
 
-// Armazena caractere digitado na serial
-char valor[1];
+Antena antena(8,7);
 
-// Armazena os dados enviados
-int dados[1];
+char dado[1];
 
-// Inicializa o NRF24L01 nos pinos 9 (CE) e 10 (CS) do Arduino Uno
-RF24 radio(9,10);
+void setup() {
+	Serial.begin(9600);
 
-// Define o endereco para comunicacao entre os modulos
-const uint64_t pipe = 0xE13CBAF433LL;
-
-void setup()
-{
-  // Inicializa a serial
-  Serial.begin(57600);
-  Serial.println("Digite 1, 2 ou L e pressione ENVIAR...");
-  
-  // Inicializa a comunicacao do NRF24L01
-  radio.begin();
-  // Entra em modo de transmissao
-  radio.openWritingPipe(pipe);
+	antena.ligar();
+	antena.set_endereco(0xAABBCCDDEEFF);
+	antena.iniciar_modo_escrita();
 }
 
 void loop() {
-	dados[0] = 1;
-	Serial.println(radio.write(dados, 1));
-	delay(100);
+	int potenciometro;
+	potenciometro = analogRead(A1);
+
+	if (potenciometro < 200) {
+		dado[0] = 'h';
+	} else if (potenciometro < 400) {
+		dado[0] = 'd';
+	} else {
+		dado[0] = 'a';
+	}
+
+	antena.escrever(dado);
+
+	delay(1000);
 }

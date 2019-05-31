a2bdb3ef8e80659bd947ec4328efee49dc02b90e
Ricardo Boing <ricardoboing.ufsc@gmail.com>
2019-05-31 09:09:48 -0300

    [ARDUINO] NRF24L01 substituida pela ESP8266 ESP-01. Add sensor de distancia e calculo do tempo para o retorno do no iot (ao ponto de partida/origem).

--- Fonte/Arduino/Carrinho/Carrinho.ino
+++ Fonte/Arduino/Carrinho/Carrinho.ino
@@ -1,43 +1,104 @@
 #include "Carrinho.hpp"
-#include "Antena.hpp"
-
-Antena antena(8,7);
-Carrinho carrinho(3, 4, 6, 5, 1, 2, 9, 10);
+#include <SoftwareSerial.h>
+#include <Ultrasonic.h>
+SoftwareSerial antennaEsp(2,3);
+Carrinho carrinho(7, 4, 6, 5, 1, 8, 9, 10);
+Ultrasonic ultrasonic(4, 5);
 
 int estado = 0;
-int contador = 0;
+unsigned long tempoInicial = 0;
+unsigned long tempoFinal = 0;
+unsigned long intervaloEntreTempos = 0;
+unsigned long tempoVoltaEsperado = 0;
 
 void setup() {
-	Serial.begin(9600);
+	Serial.begin(115200);
+    antennaEsp.begin(115200);
+    esp_configure();
 }
+void esp_configure() {
+    // Configuracoes iniciais
+    esp_data_send("RST");
+    esp_data_send("CWMODE=1");
+    esp_data_send("CWJAP=\"GVT-DFDC\",\"S1F7586408\"");
+    
+    // Mostra ip do "no" dentro da rede
+    esp_data_send("CIFSR");
 
+    while(!antennaEsp.find("OK"));
+
+    // Registra "no" iot no servidor
+    esp_data_send("CIPSTART=\"TCP\",\"192.168.25.16\",8081");
+    delay(2000);
+
+    // Fecha conexao com o servidor
+    esp_data_send("CIPCLOSE");
+    delay(2000);
+
+    // Cria servidor socket na porta 8080
+    esp_data_send("CIPMUX=1");
+    esp_data_send("CIPSERVER=1,8080");
+    delay(2000);
+}
+void esp_data_send(String comando) {
+    // Todo comando deve iniciar com "AT+" e terminar com "\r\n"
+    comando = "AT+"+comando+"\r\n";
+  
+    String resposta;
+    antennaEsp.print(comando);
+  
+    delay(2000);
+
+    while(antennaEsp.available()) {
+        char data = antennaEsp.read();
+        resposta += data;
+    }
+  
+    Serial.print(resposta);
+}
 void loop() {
-	char valor[] = "a";
-
-	// Carrinho parado: verifica comando recebido (antena)
+    // Carrinho parado: verifica comando recebido (antena)
 	if (estado == 0) {
-		Serial.println(contador);
-		if (valor[0] == 'a') {
+		if (antennaEsp.available()) {
+            delay(2000);
+            while (antennaEsp.available()) antennaEsp.read();
+            Serial.println("available");
 			carrinho.andar_sentido_sul();
 			carrinho.abrir_reservatorio();
 
 			estado = 1;
-			contador = 0;
-		}
+            tempoInicial = millis();
+        }
 	// Carrinho andando (sentido 1 - ida)
-	} else if (estado == 1) {
-		contador++;
-		if (contador > 15) {
-			estado = 2;
-		}
+    } else if (estado == 1) {
+        long microsec = ultrasonic.timing();
+        float distanciaCentimetro = ultrasonic.convert(microsec, Ultrasonic::CM);
+        
+        if (distanciaCentimetro < 15 && distanciaCentimetro > 0) {
+            tempoFinal = millis();
+            carrinho.parar();
+            estado = 2;
+
+            intervaloEntreTempos = tempoFinal - tempoInicial;
+            tempoVoltaEsperado = tempoFinal + intervaloEntreTempos;
+
+            carrinho.andar_sentido_norte();
+            delay(intervaloEntreTempos);
+        }
 	// Carrinho andando (sentido 2 - volta)
 	} else {
-		contador--;
-		if (contador <= 0) {
-			estado = 0;
-		}
-	}
+		unsigned long tempoAtual = millis();
+        if (tempoVoltaEsperado <= tempoAtual) {
+            estado = 0;
 
-	//Serial.print(estado);
-	
+            carrinho.parar();
+            carrinho.fechar_reservatorio();
+
+            Serial.print("Retornou: ");
+            Serial.print(tempoVoltaEsperado);
+            Serial.print(" | tempoFFinal: ");
+            Serial.println(tempoAtual);
+        }
+        Serial.println(tempoAtual);
+	}
 }
